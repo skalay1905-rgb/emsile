ce90a1582162cfedf52a1591a154a8264339bf92
skalay1905-rgb <s.kalay1905@gmail.com>
2026-09-19 15:34:16 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var C='emsile-5260d00ab0';
+var C='emsile-850d0a06da';
 self.addEventListener('install',function(e){
  e.waitUntil(caches.open(C).then(function(c){return c.addAll(['./']);}).then(function(){return self.skipWaiting?null:null;}));
 });
